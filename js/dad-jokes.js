aa518ebd5d9d32e3d0074c73caaaee7f8e5d21c5
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-11-04 15:36:11 -0600

    modified code so that you can click out of a joke

--- js/dad-jokes.js
+++ js/dad-jokes.js
@@ -1,14 +1,17 @@
 var secondClick = false;
+var alreadyClicked = false;
 
 function newJoke() {
     fetch("https://icanhazdadjoke.com/", {headers: {Accept:"application/json"}})
     .then(response => response.json())
     .then(data => {
-        if (secondClick)
+        if (secondClick) {
+            alreadyClicked = true;
             document.getElementById("dad-joke").textContent = data.joke;
+        } 
         else {
             document.getElementById("dad-joke").textContent = "\"a joke.\"";
-            setTimeout(() => document.getElementById("dad-joke").textContent = "Okay, click me again, I'll give you one for real this time.", 4000);
+            setTimeout(() => !alreadyClicked ? document.getElementById("dad-joke").textContent = "Okay, click me again, I'll give you one for real this time." : console.log("User clicked out of message."), 4000);
             secondClick = true;
         }
     });
