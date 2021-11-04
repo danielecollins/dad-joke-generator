971a56a9e25505ce16339d1f59985db65a504baa
danielecollins <57637750+danielecollins@users.noreply.github.com>
2021-11-04 14:32:37 -0600

    Added a little joke at the beginning

--- js/dad-jokes.js
+++ js/dad-jokes.js
@@ -1,5 +1,15 @@
+var secondClick = false;
+
 function newJoke() {
     fetch("https://icanhazdadjoke.com/", {headers: {Accept:"application/json"}})
     .then(response => response.json())
-    .then(data => document.getElementById("dad-joke").textContent = data.joke);
+    .then(data => {
+        if (secondClick)
+            document.getElementById("dad-joke").textContent = data.joke;
+        else {
+            document.getElementById("dad-joke").textContent = "\"a joke.\"";
+            setTimeout(() => document.getElementById("dad-joke").textContent = "Okay, click me again, I'll give you one for real this time.", 4000);
+            secondClick = true;
+        }
+    });
 }
